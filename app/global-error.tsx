62ffc690e6fcb038e77f0b668cfe76b5e890e23e
Divyansh Vishwakarma <divyanshvishwakarma4@gmail.com>
2024-08-13 01:00:48 +0530

    final

---
+++ app/global-error.tsx
@@ -0,0 +1,14 @@
+"use client";
+import NextError from "next/error";
+
+export default function GlobalError({ error }: { error: Error & { digest?: string } }) {
+
+
+  return (
+    <html>
+      <body>
+        <NextError statusCode={0} />
+      </body>
+    </html>
+  );
+}
---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [{ protocol: 'https', hostname: 'img.clerk.com' }]
+  }
+};
+
+export default nextConfig
